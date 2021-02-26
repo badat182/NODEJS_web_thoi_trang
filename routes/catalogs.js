05fc74d0eaa5f2b8def7491ba073574152e26680
badat <badat336@gmail.com>
2021-02-26 23:44:07 +0700

    can sua loi bat dong bo va css o trang detail

--- routes/catalogs.js
+++ routes/catalogs.js
@@ -1,9 +1,8 @@
 var modelCatalogs = require('../models/catalogs'); //nhúng model vào và đặt tên model
-var modelProducts = require('../models/products');
 
 var express = require('express');
 var router = express.Router();
-// ROUTER CHO CATALOG
+
 router.get('/', function(req, res, next) {
     let listCat = modelCatalogs.list();//cách gọi hàm trong model, để có dữ liệu từ db
     res.render("admin/catalog_list", {list:listCat});
@@ -43,7 +42,5 @@ router.get('/delete/:id', function(req, res) { //tham số id để biết recor
   modelCatalogs.delete(id);
   res.redirect('/cat')
 });
-//ROUTER CHO SAN PHAM
-
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -18,12 +18,20 @@ router.get('/sp', function(req, res, next) {
 
   res.render('clients/products', {lp:listPro, lc:listCat} );
 });
-router.get('/detail/:id', function(req, res, next) {
-  let id = req.params.id;
-  let listPro = modelProducts.list();
-  let listCat = modelCatalogs.list();
+// router.get('/detail/:id', function(req, res, next) {
+//   let id = req.params.id;
+//   let listPro = modelProducts.list();
+//   let listCat = modelCatalogs.list();
 
-  res.render('clients/details', {lp:listPro, lc:listCat} );
+//   res.render('clients/details', {lp:listPro, lc:listCat} );
+// });
+
+router.get('/detail/:id', function(req, res, next) {//tham số id để biết record cần chỉnh
+  let id = req.params.id;
+
+  let listPro = modelProducts.list();
+  let detail = modelProducts.detail(id);
+  res.render('clients/details', {d:detail, lp:listPro});
 });
 
 module.exports = router;
